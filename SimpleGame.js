84fa963655d027b51b8b9ee2ca8bb4b12a1b7c1a
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-11-16 11:12:31 -0800

    movment yay!

---
+++ SimpleGame.js
@@ -0,0 +1,25 @@
+var ravenX = 250;
+document.addEventListener("keydown", function(e) {
+ if(e.keyCode == 37){
+   ravenX -=15;
+document.getElementById("raven").setAttribute("x", ravenX)
+
+ }
+ else if(e.keyCode == 39){
+       ravenX +=15;
+document.getElementById("raven").setAttribute("x", ravenX)
+ }
+})
+
+var ravenY = 250;
+document.addEventListener("keydown", function(e) {
+ if(e.keyCode == 38){
+   ravenY -=15;
+document.getElementById("raven").setAttribute("y", ravenY)
+
+ }
+ else if(e.keyCode == 40){
+       ravenY +=15;
+document.getElementById("raven").setAttribute("y", ravenY)
+ }
+})
