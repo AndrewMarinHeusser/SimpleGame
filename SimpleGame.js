18c0a576bedb564f7f7e3af39e2b7140363890a0
Andrew Marin Heusser <heusser.andrew@gmail.com>
2016-12-01 11:01:38 -0800

    got score to work
    also got animatioons to stop

--- SimpleGame.js
+++ SimpleGame.js
@@ -7,11 +7,14 @@ var foodY;
 var food2X;
 var food2Y;
 var score = 0;
+var timestart = Date.now()
 
 function randomNumber(min,max)
 {
  return Math.floor(Math.random()*(max-min+1)+min);
 }
+
+//MOVMENT
 
 var ravenX = 250;
 document.addEventListener("keydown", function(e) {
@@ -38,11 +41,12 @@ document.getElementById("raven").setAttribute("y", ravenY)
 document.getElementById("raven").setAttribute("y", ravenY)
  }
 
+//OVERLAP
 
 foodX = Number(document.getElementById("food1").getAttribute("x"))
 foodY = Number(document.getElementById("food1").getAttribute("y"))
-playerX = Number(document.getElementById("raven").getAttribute("x" + 60))
-playerY = Number(document.getElementById("raven").getAttribute("y" + 70))
+playerX = Number(document.getElementById("raven").getAttribute("x" + 80))
+playerY = Number(document.getElementById("raven").getAttribute("y" + 90))
 
 
 
@@ -51,13 +55,11 @@ console.log("eaten food");
 document.getElementById("food1").setAttribute("x",randomNumber(25,775))
   score = score + 1;
   document.getElementById("score").textContent = score
+
 }
 
 food2X = Number(document.getElementById("food2").getAttribute("x"))
 food2Y = Number(document.getElementById("food2").getAttribute("y"))
-playerX = Number(document.getElementById("raven").getAttribute("x"))
-playerY = Number(document.getElementById("raven").getAttribute("y"))
-
 
 if (playerX > food2X && playerX < food2X + width && playerY > food2Y && playerY < food2Y + height) {
 console.log("eaten food");
@@ -66,4 +68,13 @@ score = score + 1;
 document.getElementById("score").textContent = score
 }
 
+//GAME OVER
+
+if (score == 5) {
+  console.log("GAME OVER");
+  document.getElementById("canvas").pauseAnimations()
+  var timestop = Date.now()
+  var duration = timestop - timestart
+  document.getElementById("time").textContent = duration
+}
 })
